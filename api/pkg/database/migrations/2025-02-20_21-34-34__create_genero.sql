c3166c612aacefd6d7739bddaf8ccd126d3751ce
jpcadinelli <jpcadinelli@gmail.com>
2025-02-20 21:37:36 -0300

    feat: adição da migration da tabela gênero

---
+++ api/pkg/database/migrations/2025-02-20_21-34-34__create_genero.sql
@@ -0,0 +1,10 @@
+CREATE TABLE genero (
+    id UUID NOT NULL,
+    nome TEXT NOT NULL,
+    created_at TIMESTAMP,
+        CONSTRAINT pk_genero PRIMARY KEY (id)
+);
+
+COMMENT ON COLUMN genero.id IS 'Identificador único do Gênero';
+COMMENT ON COLUMN genero.titulo IS 'Nome do Gênero';
+COMMENT ON COLUMN genero.created_at IS 'Data de Criação';
