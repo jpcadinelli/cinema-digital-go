a61bb0e7e1ba969e928a3cbcf7e2a573cc38c683
jpcadinelli <jpcadinelli@gmail.com>
2025-03-23 16:07:15 -0300

    feat: adição do domain de sessão

---
+++ api/app/sessao/model/sessao.go
@@ -0,0 +1,50 @@
+package model
+
+import (
+	"cinema_digital_go/api/pkg/global"
+	"errors"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"time"
+)
+
+type Sessao struct {
+	Id              uuid.UUID `json:"id"`
+	IdFilme         uuid.UUID `json:"idFilme"`
+	IdSala          uuid.UUID `json:"idSala"`
+	DataInicio      time.Time `json:"dataInicio"`
+	DataFim         time.Time `json:"dataFim"`
+	PrecoIngresso   float64   `json:"precoIngresson"`
+	Disponibilidade int       `json:"disponibilidade"`
+}
+
+func (s *Sessao) TableName() string {
+	return global.TabelaFilme
+}
+
+func (s *Sessao) BeforeCreate(_ *gorm.DB) (err error) {
+	s.Id, err = uuid.NewV7()
+	return
+}
+
+func (s *Sessao) Validar() error {
+	if s.IdFilme == uuid.Nil {
+		return errors.New("idFilme is required")
+	}
+	if s.IdSala == uuid.Nil {
+		return errors.New("idSala is required")
+	}
+	if s.DataInicio == (time.Time{}) {
+		return errors.New("dataInicio is required")
+	}
+	if s.DataFim == (time.Time{}) {
+		return errors.New("dataFim is required")
+	}
+	if s.PrecoIngresso == 0 {
+		return errors.New("precoIngresso is required")
+	}
+	if s.Disponibilidade == 0 {
+		return errors.New("disponibilidade is required")
+	}
+	return nil
+}
