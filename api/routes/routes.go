05eec0d045ef2922600f6540ed1ce077cc83581a
jpcadinelli <jpcadinelli@gmail.com>
2025-05-25 10:25:28 -0300

    refactor: tornando em-cartaz de sessões público

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -39,7 +39,7 @@ func SetupRoutes(router *gin.Engine) *gin.Engine {
 		{
 			salaRoutes(salaGroup)
 		}
-		sessaoGroup := main.Group("/sessoes", middleware.Auth())
+		sessaoGroup := main.Group("/sessoes")
 		{
 			sessaoRoutes(sessaoGroup)
 		}
