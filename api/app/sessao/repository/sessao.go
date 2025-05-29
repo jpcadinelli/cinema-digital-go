20b92bd9510f249cbf59dd7f43542a5493ce0fd4
Pedro Coelho <pedro.coelho@dgtallab.com.br>
2025-05-28 21:11:01 -0300

    feat: subindo ordenacao

--- api/app/sessao/repository/sessao.go
+++ api/app/sessao/repository/sessao.go
@@ -91,6 +91,7 @@ func (r *sessaoRepositoryImpl) GetEmCartaz() ([]model.Sessao, error) {
 	var sessoes []model.Sessao
 
 	tx := r.db.
+		Order("data_inicio ASC").
 		Preload("Filme").
 		Preload("Sala").
 		Where("disponibilidade = ?", enum.SessaoDisponivel).
